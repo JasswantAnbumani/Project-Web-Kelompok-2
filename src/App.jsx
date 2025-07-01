60bfecd89517ef0b77411bc0fd45cafb8ed89df5
SinagaRido22 <sinagaalfridocristian@gmail.com>
2025-07-01 23:37:24 +0700

    App.jsx
    Updated app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 // App.jsx
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Navbar from './components/HomePage/Navbar';
-import Hero from './components/HomePage/Hero';
-import Articles from './components/HomePage/Article';
+import Navbar from './components/HomePage/Navbar.jsx';
+import Hero from './components/HomePage/Hero.jsx';
+import Articles from './components/HomePage/Article.jsx';
 import Footer from './components/HomePage/Footer';
 import { ThemeProvider } from './theme/ThemeContext.jsx';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { createBrowserRouter } from 'react-router-dom';
 import ShootingStars from './components/HomePage/ShootingStars.jsx';
 import ConstellationCanvas from './components/HomePage/ConstellationCanvas';
+import Signup from './pages/Login&SignUpPage/Signup.jsx';
+import Login from './pages/Login&SignUpPage/Login.jsx';
+import { AuthProvider } from './pages/Login&SignUpPage/authContext';
+import PrivateRoute from './pages/Login&SignUpPage/PrivateRoute.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BlackholeLoader from './components/HomePage/BlackholeLoader.jsx';
+import RouteChangeLoader from './components/HomePage/RouteChangeLoader.jsx';
+import ArticlesPage from './pages/ArticlesPage/ArticlePage.jsx';
+import UserProfile from './pages/UserProfile/UserProfile.jsx';
+import AboutUs from './pages/AboutUs/AboutUs.jsx';
+import CursorGlow from './components/HomePage/CursorGlow.jsx';
+import PortfolioJasswant from './pages/Portofolio/PortofolioJasswant/PortofolioJasswant.jsx';
+import PortfolioAlfrido from './pages/Portofolio/PortofolioAlfrido/PortofolioAlfrido.jsx';
+import PortfolioAgnes from './pages/Portofolio/PortofolioAgnes/PortofolioAgnes.jsx';
+import PortfolioPoda from './pages/Portofolio/PortofolioPoda/PortofolioPoda.jsx';
 
 
 function App() {
